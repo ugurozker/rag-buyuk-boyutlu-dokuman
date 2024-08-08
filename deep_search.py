f70510b87dc6195d03794b4856a9ab8174ef9c33
ceydahamurcu95 <ceydahamurcu95@gmail.com>
2024-08-08 13:49:39 +0300

    fix

--- deep_search.py
+++ deep_search.py
@@ -32,17 +32,11 @@ load_dotenv()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-file_handler = logging.FileHandler('deepsearch_table_extraction.log')
-file_handler.setLevel(logging.DEBUG)
-
 console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
-console_handler.setFormatter(formatter)
 
-logger.addHandler(file_handler)
-logger.addHandler(console_handler)
+console_handler.setFormatter(formatter)
 
 class TableExtractor:
     @staticmethod
